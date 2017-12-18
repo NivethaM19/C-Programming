3d0f9234cf3c1456e5ea8eb33090dd45a2ddf220
Nivetha Manikandan <34644928+NivethaM19@users.noreply.github.com>
2017-12-18 21:49:29 +0530

    Create concatenate.c

---
+++ Basics-part3/concatenate.c
@@ -0,0 +1,19 @@
+//concatenate without builtin functions
+#include <stdio.h>
+
+int main(void)
+{
+   char str1[50], str2[50];
+   int i, j;
+   scanf("%s",str1);
+   scanf("%s",str2);
+   for(i=0; str1[i]!='\0'; ++i);//this stores the length of string1
+   for(j=0; str2[j]!='\0'; ++j, ++i)
+   {
+      str1[i]=str2[j];
+   }
+   str1[i]='\0';
+   printf("\nOutput: %s",str1);
+   return 0;
+
+}
