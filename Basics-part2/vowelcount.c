2e31eb19f4e9c78f93e3bff9e920f4b4ea6bfdc6
Nivetha Manikandan <34644928+NivethaM19@users.noreply.github.com>
2017-12-18 21:47:18 +0530

    Update vowelcount.c

--- Basics-part2/vowelcount.c
+++ Basics-part2/vowelcount.c
@@ -1,1 +1,16 @@
 //to count the numberof vowels in a string
+#include <stdio.h>
+
+int main(void) 
+{
+	char a[20];
+	int i,count=0;
+	scanf("%s",a);
+	for(i=0;a[i]!='\0';i++)
+	{
+		if(a[i]=='a' || a[i]=='e' ||a[i]=='i' ||a[i]=='o' ||a[i]=='u')
+			count++;
+	} 
+	printf("%d",count);
+	return 0;
+}
